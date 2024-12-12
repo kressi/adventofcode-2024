2828135a0543781fb134a70ad139055a66a9ba7d
Michael Kressibucher <mkressibucher@hotmail.com>
2024-12-12 23:23:04 +0100

    Solve day 12 b

--- 12/day_12_b.py
+++ 12/day_12_b.py
@@ -1,42 +1,125 @@
+
 import sys
 import re
 import itertools
 
 sys.setrecursionlimit(10**6)
 
-DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+
+DIRECTIONS = [(0,1), (0,-1), (1,0), (-1,0)]
+
 
 def main(file):
-    tmap = read_data(file)
+    garden = read_data(file)
+    r = regions(garden)
     total = 0
-    for i, line in enumerate(tmap):
-        for j, h in enumerate(line):
-            if h == 0:
-                scores = [score(tmap, i, j, d) for  d in DIRECTIONS]
-                total += sum(scores)
+    for c, points in r:
+        area = len(points)
+        p = perimeter(points, len(garden), len(garden[0]))
+        print(c, area, p, points)
+        total += area * p
     print(total)
 
-def score(tmap, i0, j0, d):
-    i1 = i0 + d[0]
-    j1 = j0 + d[1]
-    if not in_field(tmap, i1, j1):
-        return 0
-    h0 = tmap[i0][j0]
-    h1 = tmap[i1][j1]
-    if h0 + 1 != h1:
-        return 0
-    if h1 == 9:
-        return 1
-    return sum([score(tmap, i1, j1, d) for  d in DIRECTIONS])
 
-def in_field(tmap, i, j):
-    return i >= 0 and i < len(tmap) and j >= 0 and j < len(tmap[0])
+def regions(garden):
+    r = []
+    for i, _ in enumerate(garden):
+        for j, _ in enumerate(garden[0]):
+            current = garden[i][j]
+            if current == ".":
+                continue
+            garden[i][j] = "."
+            r.append((current, [(i, j)] + list(itertools.chain.from_iterable([region(garden, current, i+dx, j+dy) for dx, dy in DIRECTIONS]))))
+    return r
+
+
+def region(garden, c0, i1, j1):
+    if c0 == ".":
+        return []
+    if not in_garden(garden, i1, j1):
+        return []
+    if garden[i1][j1] != c0:
+        return []
+    garden[i1][j1] = "."
+    return [(i1, j1)] + list(itertools.chain.from_iterable([region(garden, c0, i1+dx, j1+dy) for dx, dy in DIRECTIONS]))
+
+
+def perimeter(points, len_i, len_j):
+    total = 0
+    upper_border = []  # ( 0, 1)
+    lower_border = []  # ( 0,-1)
+    left_border = []   # (-1, 0)
+    right_border = []  # ( 1, 0)
+    for i0, j0 in points:
+        for di, dj in DIRECTIONS:
+            i1 = i0 + di
+            j1 = j0 + dj
+            if (i1, j1) in points:
+                continue
+            if di == 0 and dj == 1:
+                right_border.append((i0, j0))
+            elif di == 0 and dj == -1:
+                left_border.append((i0, j0))
+            elif dj == 0 and di == -1:
+                upper_border.append((i0, j0))
+            elif dj == 0 and di == 1:
+                lower_border.append((i0, j0))
+            else:
+                print(i0, j0, di, dj)
+                assert False
+    
+    xi = None
+    xj = None
+    left_border.sort(key=second)
+    #print("left ", left_border)
+    for i, j in left_border:
+        if j != xj or i != xi + 1:
+            total += 1
+        xi = i
+        xj = j
+    xi = None
+    xj = None
+    right_border.sort(key=second)
+    #print("right", right_border)
+    for i, j in right_border:
+        if j != xj or i != xi + 1:
+            total += 1
+        xi = i
+        xj = j
+    xi = None
+    xj = None
+    upper_border.sort(key=first)
+    #print("up   ", upper_border)
+    for i, j in upper_border:
+        if i != xi or j != xj + 1:
+            total += 1
+        xi = i
+        xj = j
+    xi = None
+    xj = None
+    lower_border.sort(key=first)
+    #print("down ",lower_border)
+    for i, j in lower_border:
+        if i != xi or j != xj + 1:
+            total += 1
+        xi = i
+        xj = j
+    return total
+
+def first(val):
+    return val[0]*10**6+val[1]
+
+def second(val):
+    return val[1]*10**6+val[0]
+
+def in_garden(garden, i, j):
+    return i >= 0 and i < len(garden) and j >= 0 and j < len(garden[0])
+
 
 def read_data(file):
     with open(file) as f:
         tmap = f.read().splitlines()
-    return [[int(h) for h in line] for line in tmap]
-
+    return [[c for c in line] for line in tmap]
 
 
 if __name__ == "__main__":
